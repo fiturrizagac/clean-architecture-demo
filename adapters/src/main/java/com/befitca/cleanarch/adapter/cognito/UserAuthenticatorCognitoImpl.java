869135eb909f1e3ca6825b3f8096b33ec57444c7
Franco Iturrizaga <fiturrizagac@gmail.com>
2020-01-17 23:25:48 -0500

    Adding first cognito integration

--- adapters/src/main/java/com/befitca/cleanarch/adapter/cognito/UserAuthenticatorCognitoImpl.java
+++ adapters/src/main/java/com/befitca/cleanarch/adapter/cognito/UserAuthenticatorCognitoImpl.java
@@ -1,11 +1,110 @@
 package com.befitca.cleanarch.adapter.cognito;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
+import com.amazonaws.services.cognitoidp.model.*;
 import com.befitca.cleanarch.usecase.port.out.UserAuthenticator;
+import lombok.Setter;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class UserAuthenticatorCognitoImpl implements UserAuthenticator {
 
+    private String region;
+    private String clientId;
+    @Setter
+    private String clientSecret;
+    private String poolId;
+    private AWSCredentials credentials;
+    private AWSCognitoIdentityProvider awsCognitoIdentityProvider;
+
+    public UserAuthenticatorCognitoImpl(String clientId, String poolId, String region, AWSCredentials credentials) {
+        this.region = region;
+        this.clientId = clientId;
+        this.credentials = credentials;
+        this.poolId = poolId;
+        this.awsCognitoIdentityProvider =  createCognitoClient();
+    }
+
+    protected AWSCognitoIdentityProvider createCognitoClient() {
+        AWSCognitoIdentityProvider awsCognitoIdentityProvider = AWSCognitoIdentityProviderClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(Regions.fromName(this.region))
+                .build();
+        return awsCognitoIdentityProvider;
+    }
+
     @Override
     public boolean authenticate(final String username, final String password) {
+
+        AuthenticationResultType authentication = null;
+
+        try {
+            AdminInitiateAuthResult adminInitiateAuthResult = authenticateWithCognito(username, password);
+            if (ChallengeNameType.NEW_PASSWORD_REQUIRED.toString().equals(adminInitiateAuthResult.getChallengeName())) {
+                String session = adminInitiateAuthResult.getSession();
+                AdminRespondToAuthChallengeResult adminRespondToAuthChallengeResult = confirmPasswordWithCognito(username,password,session);
+                authentication = adminRespondToAuthChallengeResult.getAuthenticationResult();
+            } else {
+                authentication = adminInitiateAuthResult.getAuthenticationResult();
+            }
+        } catch (final Exception ex) {
+            ex.printStackTrace();
+        }
         return false;
+    }
+
+    protected AdminRespondToAuthChallengeResult confirmPasswordWithCognito(final String username, final String password, final String session) {
+
+        AdminRespondToAuthChallengeRequest adminRespondToAuthChallengeRequest = new AdminRespondToAuthChallengeRequest();
+        adminRespondToAuthChallengeRequest.setUserPoolId(poolId);
+        adminRespondToAuthChallengeRequest.setClientId(clientId);
+        adminRespondToAuthChallengeRequest.setSession(session);
+        adminRespondToAuthChallengeRequest.setChallengeName(ChallengeNameType.NEW_PASSWORD_REQUIRED);
+        adminRespondToAuthChallengeRequest.addChallengeResponsesEntry("USERNAME",username);
+        adminRespondToAuthChallengeRequest.addChallengeResponsesEntry("PASSWORD",password);
+        adminRespondToAuthChallengeRequest.addChallengeResponsesEntry("NEW_PASSWORD",password);
+
+        AdminRespondToAuthChallengeResult adminRespondToAuthChallengeResult = awsCognitoIdentityProvider.adminRespondToAuthChallenge(adminRespondToAuthChallengeRequest);
+        return adminRespondToAuthChallengeResult;
+    }
+
+    private AdminInitiateAuthResult authenticateWithCognito(String username, String password) {
+
+        AdminInitiateAuthRequest adminInitiateAuthRequest = new AdminInitiateAuthRequest();
+        adminInitiateAuthRequest.setAuthFlow(AuthFlowType.ADMIN_USER_PASSWORD_AUTH);
+        adminInitiateAuthRequest.setClientId(this.clientId);
+        adminInitiateAuthRequest.setUserPoolId(this.poolId);
+        if (clientSecret != null) {
+            adminInitiateAuthRequest.addAuthParametersEntry("SECRET_HASH", calculateSecretHash(poolId,clientSecret,clientId));
+        }
+        adminInitiateAuthRequest.addAuthParametersEntry("USERNAME", username);
+        adminInitiateAuthRequest.addAuthParametersEntry("PASSWORD", password);
+        AdminInitiateAuthResult adminInitiateAuthResult = awsCognitoIdentityProvider.adminInitiateAuth(adminInitiateAuthRequest);
+        return adminInitiateAuthResult;
+    }
+
+    public static String calculateSecretHash(String userPoolClientId, String userPoolClientSecret, String userName) {
+        final String HMAC_SHA256_ALGORITHM = "HmacSHA256";
+
+        SecretKeySpec signingKey = new SecretKeySpec(
+                userPoolClientSecret.getBytes(StandardCharsets.UTF_8),
+                HMAC_SHA256_ALGORITHM);
+        try {
+            Mac mac = Mac.getInstance(HMAC_SHA256_ALGORITHM);
+            mac.init(signingKey);
+            mac.update(userName.getBytes(StandardCharsets.UTF_8));
+            byte[] rawHmac = mac.doFinal(userPoolClientId.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(rawHmac);
+        } catch (Exception e) {
+            throw new RuntimeException("Error while calculating ");
+        }
     }
 }
--- adapters/src/test/java/com/befitca/cleanarch/adapter/cognito/UserAuthenticatorCognitoImplTest.java
+++ adapters/src/test/java/com/befitca/cleanarch/adapter/cognito/UserAuthenticatorCognitoImplTest.java
@@ -1,20 +1,33 @@
 package com.befitca.cleanarch.adapter.cognito;
 
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
 import org.junit.Before;
 import org.junit.Test;
 
 public class UserAuthenticatorCognitoImplTest {
 
+    private static final String REGION = "xxxxxx";
+    private static final String CLIENT_ID = "xxxxxx";
+    private static final String CLIENT_SECRET = "xxxxxx";
+    private static final String POOL_ID = "xxxxxx";
+    private static final String AWS_ACCESSKEY = "xxxxxx";
+    private static final String AWS_SECRETKEY = "xxxxxx";
+    private static final String USERNAME = "xxxxxx";
+    private static final String PASSWORD = "xxxxxx";
+
     private UserAuthenticatorCognitoImpl target;
 
     @Before
     public void setUp() {
-        target = new UserAuthenticatorCognitoImpl();
+        AWSCredentials credentials = new BasicAWSCredentials(AWS_ACCESSKEY,AWS_SECRETKEY);
+        target = new UserAuthenticatorCognitoImpl(CLIENT_ID,POOL_ID,REGION,credentials);
+        //target.setClientSecret(CLIENT_SECRET);
     }
 
     @Test
     public void name() {
-        target.authenticate("username","password");
+        target.authenticate(USERNAME,PASSWORD);
     }
 }
