3138c768cd203c4ff8842408ef45472803e74bbb
Franco Iturrizaga <fiturrizagac@gmail.com>
2020-02-09 12:05:19 -0500

    Adding reactive endpoint

--- adapters/src/main/java/com/befitca/cleanarch/adapter/web/rx/LoginRxController.java
+++ adapters/src/main/java/com/befitca/cleanarch/adapter/web/rx/LoginRxController.java
@@ -1,34 +1,28 @@
-package com.befitca.cleanarch.adapter.web;
+package com.befitca.cleanarch.adapter.web.rx;
 
 import com.befitca.cleanarch.usecase.port.in.LoginCredentials;
 import com.befitca.cleanarch.usecase.port.in.LoginUseCase;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/authentications")
-public class LoginRestController {
+@RequestMapping("/authentications/v2")
+public class LoginRxController {
 
     private LoginUseCase loginUseCase;
 
-    public LoginRestController(LoginUseCase loginUseCase) {
+    public LoginRxController(LoginUseCase loginUseCase) {
         this.loginUseCase = loginUseCase;
     }
 
     @PostMapping
-    public ResponseEntity login(final RequestEntity<LoginCredentials> loginCredentials) {
-
+    public Mono login(final RequestEntity<LoginCredentials> loginCredentials) {
         loginUseCase.login(loginCredentials.getBody());
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping
-    public String hello() {
-        return "Greetings from Adapters!";
+        return Mono.just(ResponseEntity.ok().build());
     }
 
 }
